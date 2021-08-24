94436803537a7ee8da7dfcc2b6af670b5157b530
akon47 <akon47@naver.com>
2021-08-24 17:05:16 +0900

    초기 커밋.

---
+++ SimpleTcp/Server/BinaryTcpServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTcp.Server
+{
+    public class BinaryTcpServer
+    {
+        /// <summary>
+        /// Opened Server Port
+        /// </summary>
+        public int Port { get; private set; }
+    }
+}
