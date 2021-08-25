796b033c02a5849bd828e177a8d07fc4b21e00c9
akon47 <akon47@naver.com>
2021-08-25 15:30:08 +0900

    style: 기존 탭을 공백으로 변경

--- Examples/EchoClient/Program.cs
+++ Examples/EchoClient/Program.cs
@@ -8,40 +8,42 @@ using SimpleTcp.Client;
 
 namespace EchoClient
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             using (RawTcpClient tcpClient = new RawTcpClient())
-			{
-				tcpClient.Connected += (sender, e) => Console.WriteLine($"Connect to [{e.RemoteEndPoint}]"); 
-				tcpClient.Disconnected += (sender, e) => Console.WriteLine($"{Environment.NewLine}Disconnected from [{e.RemoteEndPoint}]");
-				tcpClient.DataReceived += (sender, e) =>
-				{
+            {
+                tcpClient.Connected += (sender, e) =>
+                    Console.WriteLine($"Connect to [{e.RemoteEndPoint}]");
+                tcpClient.Disconnected += (sender, e) =>
+                    Console.WriteLine($"{Environment.NewLine}Disconnected from [{e.RemoteEndPoint}]");
+                tcpClient.DataReceived += (sender, e) =>
+                {
                     if (sender is RawTcpClient rawTcpClient)
                     {
                         byte[] readBytes = rawTcpClient.ReadExisting(); // read all data
                         Console.WriteLine($"DataReceived: {Encoding.ASCII.GetString(readBytes)}");
                     }
-				};
+                };
 
-				try
-				{
-					tcpClient.Connect("127.0.0.1", 5000);
+                try
+                {
+                    tcpClient.Connect("127.0.0.1", 5000);
 
-                    while(true)
+                    while (true)
                     {
                         string line = Console.ReadLine();
                         byte[] buffer = Encoding.ASCII.GetBytes(line);
                         tcpClient.Write(buffer, 0, buffer.Length);
                     }
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
                     Console.ReadLine();
-				}
-			}
-		}
-	}
+                }
+            }
+        }
+    }
 }
