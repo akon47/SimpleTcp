863b1ef945163b949605e1b71ea6b4c9b4c134a7
김환 <akon47@naver.com>
2021-08-28 11:41:38 +0900

    feat, refactor: Http 서버 만들기 시작, 클래스 기능별 폴더 나눔

---
+++ SimpleTcp/Server/Http/HttpServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTcp.Server
+{
+    public class HttpServer : BaseTcpServer
+    {
+        #region Public Member
+        /// <summary>
+        /// Called when request from client.
+        /// </summary>
+		public event HttpRequestEventHandler HttpRequest;
+        #endregion
+
+        #region Public Methods
+
+        #region Constructor
+        /// <summary>
+        /// RawTcpServer
+        /// </summary>
+        /// <param name="port">If you specify a valid port, the server starts immediately.</param>
+        public HttpServer(int port = -1) : base(port) { }
+        #endregion
+
+        public void Start()
+        {
+            base.Start(80);
+        }
+        #endregion
+
+        #region Protected Methods
+        protected override void OnDataReceived(IClient client, int receivedSize)
+        {
+            
+        }
+        #endregion
+    }
+}
---
+++ SimpleTcp/Server/Http/HttpServerEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTcp.Server
+{
+    public class HttpRequestEventArgs : EventArgs
+    {
+
+    }
+    public delegate void HttpRequestEventHandler(object sender, HttpRequestEventArgs e);
+}
