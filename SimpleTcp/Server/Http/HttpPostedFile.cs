9a44436a15d953406a6bed9658d8fc395e3eff83
김환 <akon47@naver.com>
2021-08-31 22:29:53 +0900

    feat: Added HttpPostedFile Class

---
+++ SimpleTcp/Server/Http/HttpPostedFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SimpleTcp.Server.Http
+{
+    public class HttpPostedFile
+    {
+        public string FileName { get; private set; }
+        public string ContentType { get; private set; }
+        public Stream FileStream { get; private set; }
+    }
+}
